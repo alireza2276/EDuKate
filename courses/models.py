bb39d13671a154829077f742a068279a9a950bbc
alireza <alirezaii802@gmail.com>
2022-09-18 18:58:24 +0430

    add courses list and detail template and view

--- courses/models.py
+++ courses/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from tinymce.models import HTMLField
 
 
@@ -25,6 +26,9 @@ class Course(models.Model):
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('courses_detail', args=[self.pk])
 
     def __str__(self):
         return f"{self.title} - {self.author} - {self.price}"
--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Course
 
-# Create your views here.
+
+class CourseListView(ListView):
+    model = Course
+    template_name = 'courses/courses-list.html'
+    context_object_name = 'courses'
+
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/courses-detail.html'
+    context_object_name = 'course'
+
+
+
